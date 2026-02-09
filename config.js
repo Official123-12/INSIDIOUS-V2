b1af95ab87349d6893e2ae1de5bce2f57b06fdd6
Loving Programming <officialstanlee143@gmail.com>
2026-02-09 13:23:54 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,20 @@
 module.exports = {
     botName: "ɪɴꜱɪᴅɪᴏᴜꜱ: ᴛʜᴇ ʟᴀꜱᴛ ᴋᴇʏ",
     ownerName: "ꜱᴛᴀɴʏᴛᴢ",
-    ownerNumber: "255xxxxxxxxx", // Without +
+    ownerNumber: "255xxxxxxxxx", 
     prefix: ".", 
-    mongodb: "YOUR_MONGODB_URL", // Required for Always Online
+    mongodb: "YOUR_MONGODB_URL", 
     newsletterJid: "120363404317544295@newsletter",
     groupJid: "120363406549688641@g.us",
-    footer: "© 2025 ꜱᴛᴀɴʏᴛᴢ ᴛᴇᴄʜ",
+    channelLink: "https://chat.whatsapp.com/J19JASXoaK0GVSoRvShr4Y",
     
-    // Feature Toggles
-    autoStatus: { view: true, like: true, reply: true },
-    autoRead: true,
-    autoSave: true,
-    antiCall: true,
-    workMode: "public", // 'public' or 'private'
-    sessionName: "session"
+    // API KEYS (FREE)
+    darlynApi: "https://api.darlyn.my.id/api/", // Free Scraper API
+    aiApi: "https://text.pollinations.ai/",
+    
+    // Security Keywords
+    scamWords: ['investment', 'invest', 'ashinde', 'gift card', 'bitcoin', 'telegram.me'],
+    pornWords: ['porn', 'sex', 'xxx', 'ngono', 'video za kikubwa', 'hentai'],
+    
+    footer: "© 2025 ꜱᴛᴀɴʏᴛᴢ ᴛᴇᴄʜ"
 };
