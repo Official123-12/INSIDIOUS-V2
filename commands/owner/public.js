14093f0f2a2ae5237a987418b2fdeca163d2c99d
Loving Programming <officialstanlee143@gmail.com>
2026-02-09 14:22:23 +0300

    Update public.js

--- commands/owner/public.js
+++ commands/owner/public.js
@@ -4,6 +4,6 @@ module.exports = {
     execute: async (conn, msg, args, { from, fancy, isOwner }) => {
         if (!isOwner) return;
         config.workMode = "public";
-        conn.sendMessage(from, { text: fancy("ʙᴏᴛ ɪꜱ ɴᴏᴡ ɪɴ ᴘᴜʙʟɪᴄ ᴍᴏᴅᴇ.") });
+        conn.sendMessage(from, { text: fancy("🥀 ʙᴏᴛ ɪꜱ ɴᴏᴡ ɪɴ ᴘᴜʙʟɪᴄ ᴍᴏᴅᴇ. ᴀʟʟ ꜱᴏᴜʟꜱ ᴄᴀɴ ᴜꜱᴇ ɪᴛ.") });
     }
 };
