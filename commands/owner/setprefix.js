2b5812ff56626d442827768a8f8d5d681fd6da8e
Loving Programming <officialstanlee143@gmail.com>
2026-02-11 17:54:54 +0300

    Update setprefix.js

--- commands/owner/setprefix.js
+++ commands/owner/setprefix.js
@@ -1,10 +1,12 @@
 const config = require('../../config');
+const { fancy } = require('../../lib/tools');
+
 module.exports = {
-    name: "setprefix",
-    execute: async (conn, msg, args, { from, fancy, isOwner }) => {
+    name: "prefix",
+    execute: async (conn, msg, args, { from, isOwner }) => {
         if (!isOwner) return;
-        if (!args[0]) return msg.reply("🥀 Provide a prefix (e.g !, #, $)");
+        if (!args[0]) return msg.reply("🥀 Provide a character (e.g !, #, $)");
         config.prefix = args[0];
-        conn.sendMessage(from, { text: fancy(`🥀 ᴘʀᴇꜰɪx ᴄʜᴀɴɢᴇᴅ ᴛᴏ: ${args[0]}`) });
+        msg.reply(fancy(`🥀 ᴘʀᴇꜰɪx ᴄʜᴀɴɢᴇᴅ ᴛᴏ: ${args[0]}`));
     }
 };
