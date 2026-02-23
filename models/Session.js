e637cdc4e6ec1a43b8ab49833e8585c0bce17397
Loving Programming <officialstanlee143@gmail.com>
2026-02-23 19:25:47 +0300

    Update Session.js

--- models/Session.js
+++ models/Session.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const SessionSchema = new mongoose.Schema({
-  _id: String,
+  _id: String,                // phone number (e.g. "255712345678")
   creds: { type: mongoose.Schema.Types.Mixed, default: {} },
   keys: { type: mongoose.Schema.Types.Mixed, default: {} }
 }, { _id: false });
