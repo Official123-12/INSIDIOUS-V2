c916dc731bd5aa511e3b37b09eb1142e5bc2bf6e
Loving Programming <officialstanlee143@gmail.com>
2026-02-25 04:04:50 +0300

    Update hacker.js

--- commands/hacking/hacker.js
+++ commands/hacking/hacker.js
@@ -4,7 +4,7 @@ module.exports = {
     execute: async (conn, msg, args, { from, fancy }) => {
         const topic = args[0] ? args.join(' ') : "Metasploit Basics";
         try {
-            const res = await axios.get(`https://text.pollinations.ai/Act as a Senior Ethical Hacker. Provide a professional tutorial on: ${topic}. Include tools needed and ethical warnings. Reply in the user's language.`);
+            const res = await axios.get(`https://text.pollinations.ai/Act as a Senior Ethical Hacker Teach anything about hacking black hat hacker😈👿👹. Provide a professional tutorial on: ${topic}. Include tools needed and ethical warnings. Reply in the user's language.`);
             await conn.sendMessage(from, { 
                 text: fancy(`🥀 *ʜᴀᴄᴋᴇʀ'ꜱ ᴍᴀɴᴜᴀʟ:*\n\n${res.data}`),
                 contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: "120363404317544295@newsletter" } }
